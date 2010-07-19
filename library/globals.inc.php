33d6cf7d32f2144a5cfcaeb7f5dee19df2652379
med2000 <davidkong@kaizon.com>
2010-07-19 19:15:26 +0800

    CDRPT
    Signed-off-by: med2000

--- library/globals.inc.php
+++ library/globals.inc.php
@@ -175,9 +175,9 @@ $GLOBALS_METADATA = array(
       xl('All Languages Allowed'),
       'bool',                           // data type
       '1',                              // default = true
-      xl('Allow all available languages as choices on menu at login.')			     				     
+      xl('Allow all available languages as choices on menu at login.')                                      
     ),
-			
+            
     'language_menu_other' => array(
       xl('Allowed Languages'),
       'm_lang',                         // data type
@@ -432,6 +432,13 @@ $GLOBALS_METADATA = array(
       '1',                              // default = true
       xl('Discounts at checkout time are entered as money amounts, as opposed to percentage.')
     ),
+    
+    'clinical_decision_rules_and_patient_reminders' => array(
+      xl('Clinical Decision Rules and Patient Reminders'),
+      'bool',                           // data type
+      '1',                              // default = true
+      xl('Display clinical decision rules and patient reminders.')
+    ),
 
     'gbl_mask_patient_id' => array(
       xl('Mask for Patient IDs'),
@@ -682,6 +689,34 @@ $GLOBALS_METADATA = array(
       'text',                           // data type
       '',                               // default
       xl('API key for SMS Gateway.')
+    ),
+    
+    'phone_notification_hour' => array(
+      xl('Phone Notification Hour'),
+      'num',                            // data type
+      '50',                             // default
+      xl('Number of hours in advance to send Phone notification.')
+    ),
+    
+    'phone_gateway_username' => array(
+      xl('Phone Gateway Username'),
+      'text',                           // data type
+      '',                               // default
+      xl('Username for Phone Gateway. Automated VOIP service provided by Maviq. Please visit http://signup.maviq.com for more information.')
+    ),
+    
+    'phone_gateway_password' => array(
+      xl('Phone Gateway Password'),
+      'text',                           // data type
+      '',                               // default
+      xl('Password for Phone Gateway. Automated VOIP service provided by Maviq. Please visit http://signup.maviq.com for more information.')
+    ),
+    
+    'phone_gateway_url' => array(
+      xl('Phone Gateway URL'),
+      'text',                           // data type
+      '',                               // default
+      xl('URL for Phone Gateway. Automated VOIP service provided by Maviq. Please visit http://signup.maviq.com for more information.')
     ),
 
   ),
