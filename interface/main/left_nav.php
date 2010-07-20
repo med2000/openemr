0aff8c88f76674e65c7f0aadac04182469d70d16
med2000 <D@vid@.(none)>
2010-07-20 16:06:48 +0800

    Revert "Apply Brady's patch for patient list"
    This reverts commit be21cc554980258c6447b83cf8eca59137666b4d.

--- interface/main/left_nav.php
+++ interface/main/left_nav.php
@@ -975,7 +975,7 @@ if (!empty($reg)) {
       <li><span><?php xl('Clients','e') ?></span>
         <ul>
 	  <?php genMiscLink('RTop','rep','0',xl('List'),'reports/patient_list.php'); ?>
-	  <?php if (acl_check('patients', 'med')) genMiscLink('RTop','rep','0',xl('Clinical'),'reports/clinical_reports.php'); ?>
+          <?php if (acl_check('patients', 'med') && !$GLOBALS['disable_prescriptions']) genMiscLink('RTop','rep','0',xl('Rx'),'reports/prescriptions_report.php'); ?>
 	  <?php genMiscLink('RTop','rep','0',xl('Referrals'),'reports/referrals_report.php'); ?>
         </ul>
       </li>
